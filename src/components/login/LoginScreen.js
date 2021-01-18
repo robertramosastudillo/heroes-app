b7669d6267c0b054ed5ddd20ce0162bac7e66c9f
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2021-01-18 15:33:02 -0500

    Se termino la aplicación de heroes

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
 
-export const LoginScreen = ({history}) => {
+export const LoginScreen = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogin = () => {
-   history.push('/');
-  }
 
-    return (
-      <div className="container mt-5">
-        <h1>Login</h1>
-        <hr/>
+    const lastPath = localStorage.getItem("lastPath")|| '/';
 
-        <button className="btn btn-primary"
-        onClick={handleLogin}>
-Login
-        </button>
-      </div>
-    );
-}
+    dispatch({
+      type: types.login,
+      payload: {
+        name: "Robert",
+      },
+    });
+    history.replace(lastPath);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
